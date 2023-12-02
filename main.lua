f11e0216d7c867d4e1898757d127a9b703fb0805
Antonio-Secchin <101224741+Antonio-Secchin@users.noreply.github.com>
2023-12-01 22:07:39 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,549 @@
+pieceStructures = {
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {'i', 'i', 'i', 'i'},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 'i', ' ', ' '},
+            {' ', 'i', ' ', ' '},
+            {' ', 'i', ' ', ' '},
+            {' ', 'i', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {' ', 'o', 'o', ' '},
+            {' ', 'o', 'o', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {'j', 'j', 'j', ' '},
+            {' ', ' ', 'j', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 'j', ' ', ' '},
+            {' ', 'j', ' ', ' '},
+            {'j', 'j', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {'j', ' ', ' ', ' '},
+            {'j', 'j', 'j', ' '},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 'j', 'j', ' '},
+            {' ', 'j', ' ', ' '},
+            {' ', 'j', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {'l', 'l', 'l', ' '},
+            {'l', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 'l', ' ', ' '},
+            {' ', 'l', ' ', ' '},
+            {' ', 'l', 'l', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', ' ', 'l', ' '},
+            {'l', 'l', 'l', ' '},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {'l', 'l', ' ', ' '},
+            {' ', 'l', ' ', ' '},
+            {' ', 'l', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {'t', 't', 't', ' '},
+            {' ', 't', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 't', ' ', ' '},
+            {' ', 't', 't', ' '},
+            {' ', 't', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 't', ' ', ' '},
+            {'t', 't', 't', ' '},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 't', ' ', ' '},
+            {'t', 't', ' ', ' '},
+            {' ', 't', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {' ', 's', 's', ' '},
+            {'s', 's', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {'s', ' ', ' ', ' '},
+            {'s', 's', ' ', ' '},
+            {' ', 's', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+    {
+        {
+            {' ', ' ', ' ', ' '},
+            {'z', 'z', ' ', ' '},
+            {' ', 'z', 'z', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+        {
+            {' ', 'z', ' ', ' '},
+            {'z', 'z', ' ', ' '},
+            {'z', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+        },
+    },
+}
+
+alfabeto = {
+    A = {
+        {" ", "i", "i", "i", " "},
+        {"i", " ", " ", " ", "i"},
+        {"i", "i", "i", "i", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"}
+    },
+    B = {
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "}
+    },
+    C = {
+        {" ", "i", "i", "i", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {" ", "i", "i", "i", " "}
+    },
+    D = {
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "}
+    },
+    E = {
+        {"i", "i", "i", "i", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", "i", "i", "i", " "}
+    },
+    F = {
+        {"i", "i", "i", "i", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "}
+    },
+    G = {
+        {" ", "i", "i", "i", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", "i", "i", " "},
+        {"i", " ", " ", "i", " "},
+        {" ", "i", "i", "i", " "}
+    },
+    H = {
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", "i", "i", "i", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"}
+    },
+    I = {
+        {"i", "i", "i", "i", "i"},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "},
+        {"i", "i", "i", "i", "i"}
+    },
+    J = {
+        {" ", " ", "i", "i", "i"},
+        {" ", " ", " ", " ", "i"},
+        {" ", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", "i", "i", " "}
+    },
+    K = {
+        {"i", " ", " ", "i", " "},
+        {"i", " ", "i", " ", " "},
+        {"i", "i", " ", " ", " "},
+        {"i", " ", "i", " ", " "},
+        {"i", " ", " ", "i", " "}
+    },
+    L = {
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", "i", "i", "i", " "}
+    },
+    M = {
+        {"i", " ", " ", " ", "i"},
+        {"i", "i", " ", "i", "i"},
+        {"i", " ", "i", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"}
+    },
+    N = {
+        {"i", " ", " ", " ", "i"},
+        {"i", "i", " ", " ", "i"},
+        {"i", " ", "i", " ", "i"},
+        {"i", " ", " ", "i", "i"},
+        {"i", " ", " ", " ", "i"}
+    },
+    O = {
+        {" ", "i", "i", "i", " "},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", "i", "i", " "}
+    },
+    P = {
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", " ", " "},
+        {"i", " ", " ", " ", " "}
+    },
+    Q = {
+        {" ", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", " ", "i", " ", " "},
+        {" ", "i", "i", "i", "i"}
+    },
+    R = {
+        {"i", "i", "i", " ", " "},
+        {"i", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "},
+        {"i", " ", "i", " ", " "},
+        {"i", " ", " ", "i", " "}
+    },
+    S = {
+        {" ", "i", "i", "i", " "},
+        {"i", " ", " ", " ", " "},
+        {" ", "i", "i", " ", " "},
+        {" ", " ", " ", "i", " "},
+        {"i", "i", "i", " ", " "}
+    },
+    T = {
+        {"i", "i", "i", "i", "i"},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "}
+    },
+    U = {
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", "i", "i", " "}
+    },
+    V = {
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", " ", "i", " "},
+        {" ", " ", "i", " ", " "}
+    },
+    W = {
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", " ", " ", "i"},
+        {"i", " ", "i", " ", "i"},
+        {"i", "i", " ", "i", "i"},
+        {"i", " ", " ", " ", "i"}
+    },
+    X = {
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", " ", "i", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", "i", " ", "i", " "},
+        {"i", " ", " ", " ", "i"}
+    },
+    Y = {
+        {"i", " ", " ", " ", "i"},
+        {" ", "i", " ", "i", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", " ", "i", " ", " "}
+    },
+    Z = {
+        {"i", "i", "i", "i", "i"},
+        {" ", " ", " ", "i", " "},
+        {" ", " ", "i", " ", " "},
+        {" ", "i", " ", " ", " "},
+        {"i", "i", "i", "i", "i"}
+    },    
+}
+
+function newSequence()
+    sequence = {}
+    for pieceTypeIndex = 1, #pieceStructures do
+        local position = love.math.random(#sequence + 1)
+        table.insert(
+            sequence,
+            position,
+            pieceTypeIndex
+        )
+    end
+end
+
+
+function love.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
+
+    gridXCount = 10
+    gridYCount = 18
+
+    pieceXCount = 4
+    pieceYCount = 4
+
+    inert = {}
+    for y = 1, gridYCount do
+        inert[y] = {}
+        for x = 1, gridXCount do
+            inert[y][x] = ' '
+        end
+    end
+    pieceType = 1
+    pieceRotation = 1
+
+    pieceX = 3
+    pieceY = 0
+
+    timer = 0
+    timerLimit = 0.5
+
+    newSequence()
+
+    function newPiece()
+        pieceX = 3
+        pieceY = 0
+        pieceRotation = 1
+        pieceType = table.remove(sequence)
+    
+        if #sequence == 0 then
+            newSequence()
+        end
+    end
+
+    function reset()
+        inert = {}
+        for y = 1, gridYCount do
+            inert[y] = {}
+            for x = 1, gridXCount do
+                inert[y][x] = ' '
+            end
+        end
+
+        newSequence()
+        newPiece()
+
+        timer = 0
+    end
+
+    reset()
+    
+end
+
+function canPieceMove(testX, testY, testRotation)
+    for y = 1, pieceYCount do
+        for x = 1, pieceXCount do
+            local testBlockX = testX + x
+            local testBlockY = testY + y
+
+            if pieceStructures[pieceType][testRotation][y][x] ~= ' ' and (testBlockX < 1 or testBlockX > gridXCount or testBlockY > gridYCount or inert[testBlockY][testBlockX] ~= ' ')
+            then
+                return false
+            end
+        end
+    end
+
+    return true
+end
+
+function love.draw()
+    --Drawing the grid
+    local function drawBlock(block, x, y)
+        local colors = {
+            [' '] = {.87, .87, .87},
+            i = {.47, .76, .94},
+            j = {.93, .91, .42},
+            l = {.49, .85, .76},
+            o = {.92, .69, .47},
+            s = {.83, .54, .93},
+            t = {.97, .58, .77},
+            z = {.66, .83, .46},
+            preview = {.75, .75, .75},
+        }
+        local color = colors[block]
+        love.graphics.setColor(color)
+
+        local blockSize = 20
+        local blockDrawSize = blockSize - 1
+        love.graphics.rectangle(
+            'fill',
+            (x - 1) * blockSize,
+            (y - 1) * blockSize,
+            blockDrawSize,
+            blockDrawSize
+        )
+    end
+
+    local offsetX = 2
+    local offsetY = 5
+
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
+            drawBlock(inert[y][x], x + offsetX, y + offsetY)
+        end
+    end
+
+    for y = 1, pieceYCount do
+        for x = 1, pieceXCount do
+            local block = pieceStructures[pieceType][pieceRotation][y][x]
+            if block ~= ' ' then
+                drawBlock(block, x + pieceX + offsetX, y + pieceY + offsetY)
+            end
+        end
+    end
+    for y = 1, pieceYCount do
+        for x = 1, pieceXCount do
+            local block = pieceStructures[sequence[#sequence]][1][y][x]
+            if block ~= ' ' then
+                drawBlock('preview', x + 5, y + 1)
+            end
+        end
+    end
+end
+
+function love.keypressed(key)
+    if key == 'x' then
+        local testRotation = pieceRotation + 1
+        if testRotation > #pieceStructures[pieceType] then
+            testRotation = 1
+        end
+
+        if canPieceMove(pieceX, pieceY, testRotation) then
+            pieceRotation = testRotation
+        end
+
+    elseif key == 'z' then
+        local testRotation = pieceRotation - 1
+        if testRotation < 1 then
+            testRotation = #pieceStructures[pieceType]
+        end
+
+        if canPieceMove(pieceX, pieceY, testRotation) then
+            pieceRotation = testRotation
+        end
+
+    elseif key == 'left' then
+        local testX = pieceX - 1
+
+        if canPieceMove(testX, pieceY, pieceRotation) then
+            pieceX = testX
+        end
+
+    elseif key == 'right' then
+        local testX = pieceX + 1
+
+        if canPieceMove(testX, pieceY, pieceRotation) then
+            pieceX = testX
+        end
+
+    elseif key == 'c' then
+        while canPieceMove(pieceX, pieceY + 1, pieceRotation) do
+            pieceY = pieceY + 1
+            timer = timerLimit
+        end
+    end
+end
+
+function love.update(dt)
+    timer = timer + dt
+    if timer >= timerLimit then
+        timer = 0
+        local testY = pieceY + 1
+        if canPieceMove(pieceX, testY, pieceRotation) then
+            pieceY = testY
+        else
+            for y = 1, pieceYCount do
+                for x = 1, pieceXCount do
+                    local block =
+                        pieceStructures[pieceType][pieceRotation][y][x]
+                    if block ~= ' ' then
+                        inert[pieceY + y][pieceX + x] = block
+                    end
+                end
+            end
+
+            for y = 1, gridYCount do
+                local complete = true
+                for x = 1, gridXCount do
+                    if inert[y][x] == ' ' then
+                        complete = false
+                        break
+                    end
+                end
+                
+                if complete then
+                    for removeY = y, 2, -1 do
+                        for removeX = 1, gridXCount do
+                            inert[removeY][removeX] =
+                            inert[removeY - 1][removeX]
+                        end
+                    end
+
+                    for removeX = 1, gridXCount do
+                        inert[1][removeX] = ' '
+                    end
+                end
+            end
+
+            newPiece()
+
+            if not canPieceMove(pieceX, pieceY, pieceRotation) then
+                reset()
+            end
+        end
+    end
+end
